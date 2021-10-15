44448241755725550dd06b00bba3c6dd35a9cea6
Hailre <reeceh@stu.otc.school.nz>
2021-10-15 18:09:13 +1300

    Base Code Debugging and Refinement I) Added 'Retry' button which resets information on the canvas for better user control over the program, (adjusted positions of other labels/buttons accordingly). II) Adjusted colours and boldness of texts for better contrast for visually challenged users. III) Renamed Files (improved organization). IV) Centered the application's position on the screen. V) Added new cardselection function which takes the random words from the nextcard function and puts them into the dictionary key and value variables. VI) Improved comments on the code
    [Improvements: May need to rehash the random.shuffle section in order to prevent duplicates in the random selection.]

--- Tkinter Project.py
+++ Tkinter Project.py
@@ -3,9 +3,9 @@ from random import *
 import random
 
 root = Tk()
-
 global random_word
 
+# Create dictionary from user input
 words = {}
 while True:
     add_card_choice = input("Would you like to add a new card (y/n)? or remove the last card (r)")
@@ -26,6 +26,7 @@ while True:
 
 word1, word2 = random.choice(list(words.items()))
 
+# Create base canvas
 canvas = Canvas(
     root,
     bg="#f2f3fe",
@@ -36,11 +37,11 @@ canvas = Canvas(
     relief="ridge")
 canvas.place(x=0, y=0)
 
-# Labels
-entry0_img = PhotoImage(file=f"img_textBox0.png")
-entry0_bg = canvas.create_image(
+# Create and place entry box
+entry_image = PhotoImage(file=f"entry.png")
+entry = canvas.create_image(
     165.0, 343.5,
-    image=entry0_img)
+    image=entry_image)
 
 my_entry = Entry(
     bd=0,
@@ -55,27 +56,34 @@ my_entry.place(
 
 # noinspection PyGlobalUndefined
 def nextcard():
-    global hinter, hint_count, key
-    # Clear screen
+    global hinter, hint_count, key, value
+    # Reset screen
     canvas.itemconfig(text="", tagOrId=answer_label)
     my_entry.delete(0, END)
     canvas.itemconfig(text="", tagOrId=hint_label)
-    # Reset Hint word
     hinter = ""
     hint_count = 0
-    # Get random dictionary value and key and put it into a variable to use as itemconfig text
-    random_card = ([(k, v) for k, v in words.items()])
+    # Get random dictionary value and key and put it into a variable
+    random_card = [(k, v) for k, v in words.items()]
     random.shuffle(random_card)
     for key, value in random_card:
         pass
-    canvas.itemconfig(text=key, tagOrId=untranslated_text)
+    canvas.itemconfig(text=key.upper(), tagOrId=untranslated_text)
+
+
+def cardselection():
+    # Random words generated from the nextcard function.
+    global word1, word2
+    word1 = key
+    word2 = value
 
 
 def answer():
+    cardselection()
     if my_entry.get().capitalize() == word2:
-        canvas.itemconfig(text=f"CORRECT! {word1.upper()} MEANS {word2.upper()}", tagOrId=answer_label)
+        canvas.itemconfig(text=f"CORRECT! '{word1.upper()}' \n MEANS {word2.upper()}", tagOrId=answer_label)
     else:
-        canvas.itemconfig(text=f"INCORRECT! {word1.upper()} DOES NOT MEAN {my_entry.get().upper()}",
+        canvas.itemconfig(text=f" INCORRECT! '{word1.upper()}' \n DOES NOT MEAN \n          {my_entry.get().upper()}",
                           tagOrId=answer_label)
 
 
@@ -85,6 +93,8 @@ hint_count = 0
 
 
 def hint():
+    # Take card selection and place it onto the canvas it letter by letter.
+    cardselection()
     global hint_count
     global hinter
 
@@ -92,10 +102,33 @@ def hint():
         hinter = hinter + word2[hint_count]
         canvas.itemconfig(text=hinter, tagOrId=hint_label)
         hint_count += 1
+    else:
+        canvas.itemconfig(text="Hint already used!", tagOrId=hint_label)
 
 
-root.geometry("330x594")
-root.iconbitmap('triangles.ico')
+def retry():
+    # Reset all hint and answer data on the canvas
+    global hinter, hint_count
+    canvas.itemconfig(text="", tagOrId=answer_label)
+    my_entry.delete(0, END)
+    canvas.itemconfig(text="", tagOrId=hint_label)
+    hinter = ""
+    hint_count = 0
+
+
+# Set app location on screen
+app_width = 330
+app_height = 594
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = (screen_width/2) - (app_width/2)
+y = (screen_height/2) - (app_height/2)
+
+# Set app scale and base information
+root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
+root.iconbitmap('Logo.ico')
 root.configure(bg="#f2f3fe")
 
 background_img = PhotoImage(file=f"background.png")
@@ -103,7 +136,8 @@ background = canvas.create_image(
     165.0, 297.0,
     image=background_img)
 
-next_button_image = PhotoImage(file=f"img0.png")
+# Create and place buttons on canvas
+next_button_image = PhotoImage(file=f"next.png")
 next_button = Button(
     image=next_button_image,
     borderwidth=0,
@@ -112,11 +146,11 @@ next_button = Button(
     relief="flat")
 
 next_button.place(
-    x=41, y=472,
+    x=41, y=433,
     width=247,
     height=57)
 
-answer_button_image = PhotoImage(file=f"img1.png")
+answer_button_image = PhotoImage(file=f"answer.png")
 answer_button = Button(
     image=answer_button_image,
     borderwidth=0,
@@ -129,7 +163,7 @@ answer_button.place(
     width=107,
     height=35)
 
-hint_button_image = PhotoImage(file=f"img2.png")
+hint_button_image = PhotoImage(file=f"hint.png")
 hint_button = Button(
     image=hint_button_image,
     borderwidth=0,
@@ -142,24 +176,37 @@ hint_button.place(
     width=107,
     height=35)
 
-answer_label = canvas.create_text(
-    164.5, 180.0,
-    text="",
-    fill="#000000",
-    font=("Roboto-Bold", int(8)))
+retry_button_image = PhotoImage(file=f"retry.png")
+retry_button = Button(
+    image=retry_button_image,
+    borderwidth=0,
+    highlightthickness=0,
+    command=retry,
+    relief="flat")
 
-# Create Hint Label
-hint_label = canvas.create_text(
-    164, 275.0,
+retry_button.place(
+    x=108, y=542,
+    width=107,
+    height=35)
+
+# Create and place labels on Canvas
+answer_label = canvas.create_text(
+    165, 165.0,
     text="",
-    fill="#000000",
-    font=("Roboto-Black", int(9)))
+    fill="#9D58FF",
+    font=("Roboto-Bold", 9, 'bold'))
+
+hint_label = canvas.create_text(
+    165, 275.0,
+    text="",
+    fill="#828282",
+    font=("Roboto", 10, 'bold'))
 
 untranslated_text = canvas.create_text(
     164, 240.0,
     text="",
     fill="#000000",
-    font=("Roboto-Black", int(23)))
+    font=("Roboto-Bold", 28, 'bold'))
 
 # Run next function when program starts
 nextcard()
